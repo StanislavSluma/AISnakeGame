45c307d511465e3044c1b5db147d68649fca2124
EgrShishov <e.shishov98@yandex.ru>
2023-08-13 15:25:43 +0300

    Init project

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-# This is a sample Python script.
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+pygame.init()
 
+width = 1000
+height = 500
+screen = pygame.display.set_mode((width, height))
+running = True
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    print("mama")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    pygame.quit()
